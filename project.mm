fc8425a1c3e2b63fdde509652df1fc696d350309
Andrey Saksonov <andrey@saksonov.me>
2017-05-19 11:45:19 +0300

    project plan for pw-planner

---
+++ project.mm
@@ -0,0 +1,81 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1494871137720" ID="ID_1283210132" MODIFIED="1494872534046" TEXT="PW Planner">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1494871949400" ID="ID_161448055" MODIFIED="1494872534003" POSITION="right" TEXT="TASKS">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1494871146246" ID="ID_1516438866" MODIFIED="1494872534005" TEXT="MQ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1494871197273" ID="ID_518166131" MODIFIED="1494922447432" TEXT="Logout Action Event">
+<font NAME="SansSerif" SIZE="14"/>
+<attribute NAME="task" VALUE="logout_action_event"/>
+<attribute NAME="effort" VALUE="3d"/>
+<attribute NAME="allocate" VALUE="andreysaksonov"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1494871158122" ID="ID_655846653" MODIFIED="1494872534037" TEXT="UI">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1494871413408" ID="ID_169026157" MODIFIED="1494922456368" TEXT="Users List">
+<font NAME="SansSerif" SIZE="14"/>
+<attribute NAME="task" VALUE="users_list"/>
+<attribute NAME="effort" VALUE="2d"/>
+<attribute NAME="allocate" VALUE="andreysaksonov"/>
+</node>
+<node COLOR="#990000" CREATED="1494871435081" ID="ID_447801964" MODIFIED="1494922463207" TEXT="Projects List">
+<font NAME="SansSerif" SIZE="14"/>
+<attribute NAME="task" VALUE="projects_list"/>
+<attribute NAME="effort" VALUE="2d"/>
+<attribute NAME="allocate" VALUE="andreysaksonov"/>
+</node>
+<node COLOR="#990000" CREATED="1494871366969" ID="ID_189257622" MODIFIED="1494922484502" TEXT="Tasks List">
+<font NAME="SansSerif" SIZE="14"/>
+<attribute NAME="task" VALUE="tasks_list"/>
+<attribute NAME="effort" VALUE="2d"/>
+<attribute NAME="allocate" VALUE="andreysaksonov"/>
+</node>
+<node COLOR="#990000" CREATED="1494871360250" ID="ID_43568668" MODIFIED="1494922496957" TEXT="Login Page">
+<font NAME="SansSerif" SIZE="14"/>
+<attribute NAME="task" VALUE="login_page"/>
+<attribute NAME="effort" VALUE="2d"/>
+<attribute NAME="allocate" VALUE="andreysaksonov"/>
+</node>
+<node COLOR="#990000" CREATED="1494871738709" ID="ID_1520309831" MODIFIED="1494922506201" TEXT="Logout Action">
+<font NAME="SansSerif" SIZE="14"/>
+<attribute NAME="task" VALUE="logout_action"/>
+<attribute NAME="effort" VALUE="2d"/>
+<attribute NAME="allocate" VALUE="andreysaksonov"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1494871165835" ID="ID_387712182" MODIFIED="1494872534044" TEXT="WS">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1494871551124" ID="ID_1195759621" MODIFIED="1494922514660" TEXT="Task Entity">
+<font NAME="SansSerif" SIZE="14"/>
+<attribute NAME="task" VALUE="task_entity"/>
+<attribute NAME="effort" VALUE="3d"/>
+<attribute NAME="allocate" VALUE="andreysaksonov"/>
+</node>
+<node COLOR="#990000" CREATED="1494871719829" ID="ID_219896986" MODIFIED="1494922521995" TEXT="Project Entity">
+<font NAME="SansSerif" SIZE="14"/>
+<attribute NAME="task" VALUE="project_entity"/>
+<attribute NAME="effort" VALUE="1d"/>
+<attribute NAME="allocate" VALUE="andreysaksonov"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1494922551680" ID="ID_1588082952" MODIFIED="1494922592534" POSITION="left" TEXT="RESOURCES">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1494922592974" ID="ID_1721785261" MODIFIED="1494922802218" TEXT="Andrey Saksonov">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<attribute NAME="resource" VALUE="andreysaksonov"/>
+<attribute NAME="limits" VALUE="{ dailymax 1h weeklymax 6h }"/>
+</node>
+</node>
+</node>
+</map>
